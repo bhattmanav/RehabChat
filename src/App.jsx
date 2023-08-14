4fba8691f76b66f6657f74ac93bbb41e84c38293
bhattmanav <90620304+bhattmanav@users.noreply.github.com>
2023-08-14 17:45:06 +0930

    Setup routes so that when users want to signup/signin they are taken to respective routes. #14

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/dashboard/Dashboard";
+import AuthSignUp from "./components/authentication/signUp/AuthSignUp";
+import AuthSignIn from "./components/authentication/signIn/AuthSignIn";
 import EditConversation from "./components/editConversation/EditConversation";
+import PrivateRoute from "./components/route/PrivateRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/signup" element={<AuthSignUp />} />
+      <Route path="/signin" element={<AuthSignIn />} />
       <Route path="/edit/:id" element={<EditConversation />} />
     </Routes>
   );
